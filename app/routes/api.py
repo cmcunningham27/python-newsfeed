6caa70e3afef553aa366c48c232f55046c4446f9
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-08-21 14:00:02 -0700

    used try... except statement in signup route to handle errors, made sure connection does not remain in a pending state by rolling back the last commit

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
@@ -13,17 +14,28 @@ def signup():
     print(data)
     db = get_db()
 
-    # create a new user
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+    # try... except handles any errors
+    try:
+        # attempt to create a new user
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
-    # save in database
-    # use db.add to prep the INSERT statement and the db.commit method to officially update the database
-    db.add(newUser)
-    db.commit()
+        # save in database
+        # use db.add to prep the INSERT statement and the db.commit method to officially update the database
+        db.add(newUser)
+        db.commit()
+    except:
+        # print will print the error message in the powershell
+        print(sys.exc_info()[0])
+
+        # insert failed, so rollback and send error to front end, 500 is the status code
+        # rollback() ensures that the database won't lock up when deployed to Heroku, prevents connection from remaining in a pending state resulting in app crashing
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
+       
 
     #sends the front end JSON notation that includes the ID of the new user
     return jsonify(id = newUser.id)
